53806c7545bd3db6e96eead20dc4a7af714f4a6b
Joel Watson <existdissolve@gmail.com>
2018-01-28 20:47:29 -0600

    Added config

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    firebaseConfig: {
+        apiKey: 'AIzaSyAtBGW16qB1qetqujCnE2oXzwHy7Tz8NFg',
+        messagingSenderId: 355779476097,
+        authDomain: "feels-box.firebaseapp.com",
+        databaseURL: "https://feels-box.firebaseio.com",
+        projectId: "feels-box",
+        storageBucket: "feels-box.appspot.com"
+    },
+    user: {
+        email: 'depthdeception@hotmail.com',
+        password: 'feelsbox-server'
+    }
+};
