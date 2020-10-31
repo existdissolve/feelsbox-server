d789084fa9a338f41ef385b1864eb8a218f5a8e7
existdissolve <existdissolve@gmail.com>
2020-10-31 02:09:12 -0500

    more logging

--- app/socket.js
+++ app/socket.js
@@ -1,12 +1,15 @@
 import socketio from 'socket.io';
+import logger from 'bristol';
+import palin from 'palin';
 
 let instance;
+logger.addTarget('console').withFormatter(palin);
 
 export const init = server => {
     instance = socketio(server);
     instance.sockets.on('connection', socket => {
         socket.on('joinroom', room => {
-            console.log('room', room);
+            logger.info('room', room);
             socket.join(room);
         });
     });
