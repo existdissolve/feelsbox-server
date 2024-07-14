bd265aa5a60517f05244b7c35eaf89951bb77110
existdissolve <existdissolve@gmail.com>
2024-07-14 13:25:25 -0400

    Update socket.js

--- app/socket.js
+++ app/socket.js
@@ -9,6 +9,7 @@ export const init = server => {
     const socketInstance = socketio(server);
 
     socketInstance.on('connection', socket => {
+        logger.info('socket connection established');
         socket.on('joinroom', (room, ip, version) => {
             logger.info('room', room);
             socket.join(room);
